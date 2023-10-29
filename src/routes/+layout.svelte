7b8b4857b8fc060879654e30def51e8243e765a3
Sumiaki Kawasaki <cb_clear_bright@yahoo.co.jp>
2023-10-29 12:52:51 +0000

    ヘッダ実装

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
   import "../app.css";
+  import Header from "$lib/Header.svelte";
 </script>
 
-<slot />
+<div class="container">
+  <Header />
+  <main class="pt-16">
+    <div class="container mx-auto px-6 my-4">
+      <slot />
+    </div>
+  </main>
+</div>
